365fb7e069f0d5fc0472332e3e083e610da9c8db
sumanhong <suman0204@naver.com>
2023-12-08 10:17:26 +0900

    Initial Commit

---
+++ SeSACTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACTesting
+//
+//  Created by 홍수만 on 2023/12/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSACTesting/SeSACTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACTestingApp.swift
+//  SeSACTesting
+//
+//  Created by 홍수만 on 2023/12/08.
+//
+
+import SwiftUI
+
+@main
+struct SeSACTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
